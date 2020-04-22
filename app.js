e7a71fb857001c9f722454b3d1529300de88ec49
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-22 17:42:07 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+require('dotenv').config()
+
+//import routes
+const userRoutes = require('./routes/user');
+
+//app
+const app = express();
+
+//middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+//routes middleware
+app.use("/api", userRoutes)
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
+
+//db connection
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true}
+  )
+  .then(() => console.log('DB Connected'))
+   
+  mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+});
