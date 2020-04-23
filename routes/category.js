7cbb9731cbf7ddf19c3e59673738da27673f4f28
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-23 20:35:54 +0200

    category read

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category');
+const { create, categoryById, read } = require('../controllers/category');
 const { userById } = require('../controllers/user');
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 
+router.get('/category/:categoryId', read)
 router.post('/category/create/:userId', requireSignin, isAdmin, isAuth, create);
 
 router.param('userId', userById)
+router.param('categoryId', categoryById);
 
 module.exports = router;
