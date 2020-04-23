1a46aa35a91ee3d8470c7ce1180242250225de0c
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-23 19:36:08 +0200

    fixed the bug

--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read} = require('../controllers/product');
+const { create, productById, read, remove} = require('../controllers/product');
 const { userById } = require('../controllers/user');
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAdmin, isAuth, create);
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
 
-router.param('productId', productById)
-router.param('userId', userById)
+router.param('productId', productById);
+router.param('userId', userById);
 
 module.exports = router;
