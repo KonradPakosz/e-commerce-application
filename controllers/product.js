6cd2a0a78c359ec461838fa1f4433c172176a311
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-23 22:09:52 +0200

    List products by popularity and release

--- controllers/product.js
+++ controllers/product.js
@@ -126,3 +126,31 @@ exports.update = (req, res) => {
     });
     
 }
+
+// New and Popular products
+// New -> sort by arrival /products?sortBy=sold&order=desc&limit=4
+// Popular -> sort by sold /products?sortBy=createdAt&order=desc&limit=4
+
+// default query if no query is given
+
+exports.list = (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+    Product.find()
+    .select("-photo") //remove photo from search for quicker results
+    .populate('category')
+    .sort([[sortBy, order]])
+    .limit(limit)
+    .exec((err, products) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Products not found'
+            });
+        };
+        res.send(products);
+    })
+}
+
+
--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update} = require('../controllers/product');
+const { create, productById, read, remove, update, list} = require('../controllers/product');
 const { userById } = require('../controllers/user');
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 
@@ -9,6 +9,7 @@ router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignin, isAdmin, isAuth, create);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
+router.get('/products', list);
 
 router.param('productId', productById);
 router.param('userId', userById);
