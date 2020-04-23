dbbd0f81a87371e10b1f9d570f05e94b30c88090
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-23 20:16:52 +0200

    product update

--- controllers/product.js
+++ controllers/product.js
@@ -75,8 +75,54 @@ exports.remove = (req, res) => {
             });
         }
         res.json({
-            deletedProduct,
             message: "Product deleted successfully"
         });
     });
 };
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: "Product could not be uploaded"
+            });
+        }
+
+        //validation
+        const { name, description, price, category, quantity, shipping } = fields;
+        
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields must be filled in"
+            });
+        }
+
+        //same as create but overwrite current object instead
+        let product = req.product
+        //lodash extend method
+        product = _.extend(product, fields);
+
+        if(files.photo) {
+            console.log(files.photo);
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "File size cannot exceed 1MB"
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result)
+        });
+    });
+    
+}
