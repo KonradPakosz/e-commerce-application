5b437ccd58bf24bcdd12eaec8326d28652618d1e
KonradPakosz <42554245+KonradPakosz@users.noreply.github.com>
2020-04-24 12:29:48 +0200

    adding react

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const expressValidator = require('express-validator');
 
 require('dotenv').config()
@@ -21,6 +22,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 //routes middleware
 app.use("/api", authRoutes);
